29102d06506fd669bcc57ddbd2e7e53f3af1861d
Carlos William <carloswtmachado@gmail.com>
2022-03-13 20:32:25 -0400

    Fundo Mobile refeito com map

--- src/FundoMobile.js
+++ src/FundoMobile.js
@@ -1,11 +1,8 @@
 export default function FundoMobile(){
+	const nomes = ["home", "search-outline", "add-circle-outline", "heart-outline", "person-outline"];
 	return(
 		<div class="fundo-mobile">
-			<ion-icon name="home"></ion-icon>
-			<ion-icon name="search-outline"></ion-icon>
-			<ion-icon name="add-circle-outline"></ion-icon>
-			<ion-icon name="heart-outline"></ion-icon>
-			<ion-icon name="person-outline"></ion-icon>
+			{nomes.map(nome => <ion-icon name={nome}></ion-icon>)}
 		</div>
 	);
 }
